805c7ca7411d50d4dbbd173f259f2e5fea096fc6
Mateusz <mt.stefanowicz@gmail.com>
2024-03-16 20:06:28 +0100

    new window and prompt

--- clearify/public/content-script.js
+++ clearify/public/content-script.js
@@ -1,16 +1,37 @@
 var bubbleDOM = document.createElement('div');
 bubbleDOM.setAttribute('class', 'selection_bubble');
 document.body.appendChild(bubbleDOM);
+bubbleDOM.style.visibility = 'hidden';
+// Apply styles using JavaScript
+bubbleDOM.style.padding = '20px';
+bubbleDOM.style.backgroundColor = '#FCF5E5';
+bubbleDOM.style.color = 'black';
+bubbleDOM.style.borderRadius = '20px';
+bubbleDOM.style.boxShadow = '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)';
+bubbleDOM.style.transition = 'transform 0.3s ease';
+bubbleDOM.style.maxWidth = '700px';
+bubbleDOM.style.textAlign = 'center';
+bubbleDOM.style.border = '2px solid black';
 
+// Add hover effect
+bubbleDOM.addEventListener('mouseenter', function() {
+    bubbleDOM.style.transform = 'scale(1.05)';
+});
+
+bubbleDOM.addEventListener('mouseleave', function() {
+    bubbleDOM.style.transform = 'scale(1)';
+});
 
 document.addEventListener('mouseup', (e) => {
   const selectedText = window.getSelection().toString();
   if (selectedText.length > 0) {
-    // Send selected text to service worker
-    //chrome.runtime.sendMessage({ selectedText: selectedText });
-    console.log('content script')
-    console.log(selectedText)
-    renderBubble(e.clientX, e.clientY, selectedText);
+    const selectionRange = window.getSelection().getRangeAt(0);
+    const boundingRect = selectionRange.getBoundingClientRect();
+
+    // Calculate the position of the selected text relative to the viewport
+    const xPos = boundingRect.left + window.pageXOffset;
+    const yPos = boundingRect.top + window.pageYOffset;
+    renderBubble(xPos, yPos, selectedText);
   }
 }, false);
 
@@ -33,16 +54,26 @@ function renderBubble(mouseX, mouseY, selection) {
   const data = {
     "model": "mixtral-8x7b-32768",
     "messages": [
-        {
-            "role": "user",
-            "content": `Rephrase the following text in a way that is easy to understand for dislexic people and return nice formatted HTML text.
-            
-            ====
-            ${selection}`
-        }
+      
+      {
+        "role": "system",
+        "content": `Act as a system do rewriting text. Your only task is to rephrase and format the following text in a way that is easy to understand for dislexic people and make the text shorter without changing the context. Use active voice when rephrasing the word and it would be the best to use 60-80 characters per sentence. Avoid abbreviations where possible. Return only text. Bold the words that are important or meaning to the text.`
+      },
+      {
+          "role": "user",
+          "content": `Jamie Kalven (born 1948) is an American journalist, author, human rights activist, and community organizer based in Chicago, Illinois. He is the founder of the Invisible Institute, a non-profit journalism organization based in Chicago's South Side. His work in the city has included reporting on police misconduct and poor conditions of public housing. Kalven has been referred to as a "guerrilla journalist" by Chicago journalist Studs Terkel.[1]`
+      },
+      {
+          "role": "assistant",
+          "content": `**Jamie Kalven** (born 1948) is an **American** **journalist**, **author**, **human rights activist**, and **community organizer** based in **Chicago, Illinois**. He is the founder of the **Invisible Institute**, a non-profit **journalism organization** based in Chicago's **South Side**. His work in the city has included reporting on **police misconduct** and poor conditions of **public housing**. Kalven has been referred to as a "**guerrilla journalist**" by Chicago journalist **Studs Terkel**.`
+      },
+      {
+        "role": "user",
+        "content": selection
+      }
     ],
-    "temperature": 0.5,
-    "max_tokens": 14000,
+    "temperature": 0.8,
+    "max_tokens": 4000,
     "top_p": 1,
     "stop": null,
     "stream": false
@@ -52,18 +83,22 @@ function renderBubble(mouseX, mouseY, selection) {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': `Bearer`
+      'Authorization': `Bearer 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`
     },
     body: JSON.stringify(data)
   }).then(response => response.json())
     .then(data => {
-      console.log(data.choices[0].message.content);
-      bubbleDOM.innerHTML = data.choices[0].message.content;
+      let text = data.choices[0].message.content;
+      console.log(text);
+      // replace **some word** with <strong>some word</strong>
+      text = text.replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>');
+      // replace new lines with <br>
+      text = text.replace(/\n/g, '<br>');
+      bubbleDOM.innerHTML = text;
       bubbleDOM.style.top = mouseY + 'px';
       bubbleDOM.style.left = mouseX + 'px';
       bubbleDOM.style.visibility = 'visible';
       bubbleDOM.style.position = 'absolute';
-      bubbleDOM.style.backgroundColor = 'yellow';
     })
     .catch(error => console.error('Error:', error));
 
