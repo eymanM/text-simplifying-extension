c7b0dd4b4447b3de1a7d9f3e02fe9957d73b3034
Mateusz <mt.stefanowicz@gmail.com>
2024-03-16 16:42:59 +0100

    popover with ai msg

--- clearify/public/background.js
+++ clearify/public/background.js
@@ -30,11 +30,10 @@ chrome.runtime.onInstalled.addListener(() => {
 // Listening for a message
 chrome.runtime.onMessage.addListener((message, sender, sendCallback) => {
   console.log(message);
-  // insert the selected text into the page
   // const selection = window.getSelection();
   // const range = selection.getRangeAt(0);
   // const selectedText = range.extractContents();
-  //const span = document.createElement('span');
-  //span.appendChild(message.selectedText);
-  //span.style.backgroundColor = 'yellow';
+  // const span = document.createElement('span');
+  // span.appendChild(message.selectedText);
+  // span.style.backgroundColor = 'yellow';
 });
--- clearify/public/content-script.js
+++ clearify/public/content-script.js
@@ -1,18 +1,17 @@
+import { Button } from "../@/components/ui/button.tsx"
+
 var bubbleDOM = document.createElement('div');
 bubbleDOM.setAttribute('class', 'selection_bubble');
 document.body.appendChild(bubbleDOM);
 
 document.addEventListener('mouseup', (e) => {
   const selectedText = window.getSelection().toString();
-  console.log('dddd')
   if (selectedText.length > 0) {
     // Send selected text to service worker
-    chrome.runtime.sendMessage({ selectedText: selectedText });
+    //chrome.runtime.sendMessage({ selectedText: selectedText });
+    console.log('content script')
     console.log(selectedText)
     renderBubble(e.clientX, e.clientY, selectedText);
-  // const span = document.createElement('span');
-  // span.appendChild(selectedText);
-  // span.style.backgroundColor = 'yellow';
   }
 }, false);
 
@@ -23,8 +22,38 @@ document.addEventListener('mousedown', function (e) {
 
 // Move that bubble to the appropriate location.
 function renderBubble(mouseX, mouseY, selection) {
-  bubbleDOM.innerHTML = selection;
-  bubbleDOM.style.top = mouseY + 'px';
-  bubbleDOM.style.left = mouseX + 'px';
-  bubbleDOM.style.visibility = 'visible';
+  const data = {
+    model: "gpt-4-turbo-preview",
+    messages: [{role: "user", content: `Simplify selected text for people with dislexia in a way that is easy to understand and: 
+    
+    ====
+    ${selection}`}],
+    temperature: 0.9
+  };
+
+  <Button variant="outline" className={'conditionButton'} >{selectedText}</Button>
+  
+  // fetch('https://api.openai.com/v1/chat/completions', {
+  //   method: 'POST',
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //     'Authorization': `Bearer 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`
+  //   },
+  //   body: JSON.stringify(data)
+  // }).then(response => response.json())
+  //   .then(data => {
+  //     bubbleDOM.innerHTML = data.choices[0].message.content;
+  //     bubbleDOM.style.top = mouseY + 'px';
+  //     bubbleDOM.style.left = mouseX + 'px';
+  //     bubbleDOM.style.visibility = 'visible';
+  //     bubbleDOM.style.position = 'absolute';
+  //     bubbleDOM.style.backgroundColor = 'yellow';
+  //   })
+  //   .catch(error => console.error('Error:', error));
+  // bubbleDOM.innerHTML = selection;
+  // bubbleDOM.style.top = mouseY + 'px';
+  // bubbleDOM.style.left = mouseX + 'px';
+  // bubbleDOM.style.visibility = 'visible';
+  // bubbleDOM.style.position = 'absolute';
+  // bubbleDOM.style.backgroundColor = 'yellow';
 }
