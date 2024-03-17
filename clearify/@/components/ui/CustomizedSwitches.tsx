7d3a95ccfb7826d6a483430799261a34a99e45a5
Yussuf Nergiz <yussufnergiz@Yussufs-MacBook-Pro.local>
2024-03-17 12:32:50 +0100

    changed the switch buttons to a bigger ones

--- clearify/@/components/ui/CustomizedSwitches.tsx
+++ clearify/@/components/ui/CustomizedSwitches.tsx
@@ -4,89 +4,96 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import Switch from '@mui/material/Switch';
 import Typography from '@mui/material/Typography';
 
-// const MaterialUISwitch = styled(Switch)(({ theme }) => ({
-//     width: 62,
-//     height: 34,
-//     padding: 7,
-//     '& .MuiSwitch-switchBase': {
-//         margin: 1,
-//         padding: 0,
-//         transform: 'translateX(6px)',
-//         '&.Mui-checked': {
-//             color: '#000000',
-//             transform: 'translateX(22px)',
-//             '& .MuiSwitch-thumb:before': {
-//                 backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
-//                     '#fff',
-//                 )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
-//             },
-//             '& + .MuiSwitch-track': {
-//                 opacity: 1,
-//                 backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
-//             },
-//         },
-//     },
-//     '& .MuiSwitch-thumb': {
-//         backgroundColor: theme.palette.mode === 'dark' ? '#003892' : '#001e3c',
-//         width: 32,
-//         height: 32,
-//         '&::before': {
-//             content: "''",
-//             position: 'absolute',
-//             width: '100%',
-//             height: '100%',
-//             left: 0,
-//             top: 0,
-//             backgroundRepeat: 'no-repeat',
-//             backgroundPosition: 'center',
-//             backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
-//                 '#fff',
-//             )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
-//         },
-//     },
-//     '& .MuiSwitch-track': {
-//         opacity: 1,
-//         backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
-//         borderRadius: 20 / 2,
-//     },
-// }));
+const MaterialUISwitch = styled(Switch)(({ theme }) => ({
+    width: 62,
+    height: 34,
+    padding: 7,
+    '& .MuiSwitch-switchBase': {
+        margin: 1,
+        padding: 0,
+        transform: 'translateX(6px)',
+        '&.Mui-checked': {
+            color: '#000000',
+            transform: 'translateX(22px)',
+            '& .MuiSwitch-thumb:before': {
+                backgroundImage: `url('data:image/svg+xml;utf8,<svg width="61" height="49" viewBox="0 0 61 49" fill="none" xmlns="http://www.w3.org/2000/svg"><rect x="0.0201416" y="8.48926" width="52" height="32" rx="16" fill="#16425A"/><rect x="16.0201" y="4.48926" width="40" height="40" rx="20" fill="#0D2430" fill-opacity="0.08"/><rect x="24.0201" y="12.4893" width="24" height="24" rx="12" fill="#F7F6F4"/><path d="M34.0201 27.2692L31.2401 24.4892L30.2935 25.4292L34.0201 29.1558L42.0201 21.1558L41.0801 20.2158L34.0201 27.2692Z" fill="#FFF"/></svg>')`,
+            },
 
-const Android12Switch = styled(Switch)(({ theme }) => ({
-    padding: 8,
-    '& .MuiSwitch-track': {
-        borderRadius: 22 / 2,
-        '&::before, &::after': {
-            content: '""',
-            position: 'absolute',
-            top: '50%',
-            transform: 'translateY(-50%)',
-            width: 16,
-            height: 16,
-            color: 'black' // This will set the color of the pseudo-elements
-        },
-        '&::before': {
-            backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 24 24"><path fill="${encodeURIComponent(
-                theme.palette.getContrastText(theme.palette.primary.main),
-            )}" d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z"/></svg>')`,
-            left: 12,
-        },
-        '&::after': {
-            backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 24 24"><path fill="${encodeURIComponent(
-                theme.palette.getContrastText(theme.palette.primary.main),
-            )}" d="M19,13H5V11H19V13Z" /></svg>')`,
-            right: 12,
+
+            '& + .MuiSwitch-track': {
+                opacity: 1,
+                backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
+            },
         },
     },
     '& .MuiSwitch-thumb': {
-        boxShadow: 'none',
-        width: 16,
-        height: 16,
-        margin: 2,
+        backgroundColor: theme.palette.mode === 'dark' ? '#003892' : '#001e3c',
+        width: 32,
+        height: 32,
+        '&::before': {
+            content: "''",
+            position: 'absolute',
+            width: '100%',
+            height: '100%',
+            left: 0,
+            top: 0,
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center',
+            backgroundImage: `url('data:image/svg+xml;utf8,<svg width="61" height="49" viewBox="0 0 61 49" fill="none" xmlns="http://www.w3.org/2000/svg"><rect x="0.0201416" y="8.48926" width="52" height="32" rx="16" fill="#16425A"/><rect x="16.0201" y="4.48926" width="40" height="40" rx="20" fill="#0D2430" fill-opacity="0.08"/><rect x="24.0201" y="12.4893" width="24" height="24" rx="12" fill="#F7F6F4"/><path d="M34.0201 27.2692L31.2401 24.4892L30.2935 25.4292L34.0201 29.1558L42.0201 21.1558L41.0801 20.2158L34.0201 27.2692Z" fill="#FFF"/></svg>')`,
+        },
     },
-    '& p': {
-        color: 'black' // This will set the color of the paragraph
-    }
+    '& .MuiSwitch-track': {
+        opacity: 1,
+        backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
+        borderRadius: 20 / 2,
+    },
 }));
+
+// const Android12Switch = styled(Switch)(({ theme }) => ({
+//     padding: 8,
+//     '& .MuiSwitch-track': {
+//         borderRadius: 22 / 2,
+//         '&::before, &::after': {
+//             content: '""',
+//             position: 'absolute',
+//             top: '50%',
+//             transform: 'translateY(-50%)',
+//             width: 16,
+//             height: 16,
+//             color: 'black' // This will set the color of the pseudo-elements
+//         },
+//         '&::before': {
+//             backgroundImage: `url('data:image/svg+xml;utf8,<svg width="61" height="49" viewBox="0 0 61 49" fill="none" xmlns="http://www.w3.org/2000/svg">
+// <rect x="9.02014" y="9.48926" width="50" height="30" rx="15" fill="#F2F2F2"/>
+// <rect x="9.02014" y="9.48926" width="50" height="30" rx="15" stroke="#6D777D" stroke-width="2"/>
+// <rect x="4.02014" y="4.48926" width="40" height="40" rx="20" fill="#0D2430" fill-opacity="0.08"/>
+// <rect x="10.0201" y="10.4893" width="28" height="28" rx="14" fill="#0D2430"/>
+// <path d="M28.6868 20.7628L27.7468 19.8228L24.0201 23.5494L20.2935 19.8228L19.3535 20.7628L23.0801 24.4894L19.3535 28.2161L20.2935 29.1561L24.0201 25.4294L27.7468 29.1561L28.6868 28.2161L24.9601 24.4894L28.6868 20.7628Z" fill="#F2F2F2"/>
+// </svg>
+// ')`,
+//             left: 12,
+//         },
+//         '&::after': {
+//             backgroundImage: `url('data:image/svg+xml;utf8,<svg width="61" height="49" viewBox="0 0 61 49" fill="none" xmlns="http://www.w3.org/2000/svg">
+// <rect x="0.0201416" y="8.48926" width="52" height="32" rx="16" fill="#16425A"/>
+// <rect x="16.0201" y="4.48926" width="40" height="40" rx="20" fill="#0D2430" fill-opacity="0.08"/>
+// <rect x="24.0201" y="12.4893" width="24" height="24" rx="12" fill="#F7F6F4"/>
+// <path d="M34.0201 27.2692L31.2401 24.4892L30.2935 25.4292L34.0201 29.1558L42.0201 21.1558L41.0801 20.2158L34.0201 27.2692Z" fill="#0D2430"/>
+// </svg>
+// ')`,
+//             right: 12,
+//         },
+//     },
+//     '& .MuiSwitch-thumb': {
+//         boxShadow: 'none',
+//         width: 16,
+//         height: 16,
+//         margin: 2,
+//     },
+//     '& p': {
+//         color: 'black' // This will set the color of the paragraph
+//     }
+// }));
 
 
 
@@ -97,10 +104,10 @@ export default function CustomizedSwitches() {
             width: '100vw'
         }}>
             <FormControlLabel
-                control={<Android12Switch defaultChecked />}
+                control={<MaterialUISwitch sx={{ m: 1 }} defaultChecked /> }
                 labelPlacement={'start'}
                 label={
-                    <Typography sx={{ color: 'black', display: 'flex', justifyContent: 'space-between', fontSize: '1.3rem', marginRight: '1rem' }}>
+                    <Typography sx={{ color: 'black', display: 'flex', justifyContent: 'space-between', fontSize: '1.3rem', marginRight: '1rem' }} >
                         <strong>Bi</strong>onic&nbsp;<strong>Fo</strong>nt
                     </Typography>
                 }
@@ -110,7 +117,7 @@ export default function CustomizedSwitches() {
                 }}
             />
             <FormControlLabel
-                control={<Android12Switch defaultChecked />}
+                control={<MaterialUISwitch sx={{ m: 1 }} defaultChecked />}
                 labelPlacement={'start'}
                 label={
                     <Typography sx={{ color: 'black', display: 'flex', justifyContent: 'space-between', fontSize: '1.3rem' }}>
@@ -123,7 +130,7 @@ export default function CustomizedSwitches() {
                 }}
             />
             <FormControlLabel
-                control={<Android12Switch defaultChecked />}
+                control={<MaterialUISwitch sx={{ m: 1 }} defaultChecked />}
                 labelPlacement={'start'}
                 label={
                     <Typography sx={{ color: 'black', display: 'flex', justifyContent: 'space-between', fontSize: '1.3rem' }}>
